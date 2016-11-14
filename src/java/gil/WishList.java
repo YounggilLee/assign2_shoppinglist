5ce20b99c6aca113b2f91df06ed86e25d188c365
YounggilLee <clickbabo@gmail.com>
2016-11-14 08:14:19 -0500

    create index page. a browser issue when executing servlet. after changing response.setcontenttype to application/json. fix(?) the issue after changing  the browser to Default.

--- src/java/gil/WishList.java
+++ src/java/gil/WishList.java
@@ -6,6 +6,7 @@
 package gil;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author yglee
  */
 public class WishList extends HttpServlet {
-   
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -27,45 +28,91 @@ public class WishList extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       //for storing wishlist
-       Cookie cookie = null;
-       //get cookie array which is from client side
-       Cookie[] cookies = request.getCookies();
-       if(cookies != null){
-           //find cookie
-           for (int i = 0; i < cookies.length; ++i) {
-               if(cookies[i].getName().equals("wishList")){
-                   // found it!
-                   cookie = cookies[i];
-                   break;
-               }
-           }
-       }
-       
-       // debug
-        System.out.println(cookie);
+        
+        //prepare output
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+                
+        //for storing wishlist
+        Cookie cookie = null;
+        
+        //get cookie array which is from client side
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            
+            //find cookie
+            for (int i = 0; i < cookies.length; ++i) {
+                if (cookies[i].getName().equals("wishList")) {
+                    
+                    // found it!
+                    cookie = cookies[i]
+                            ;
+                    // debug
+                    out.println("Found Cookie: " + cookie.getValue());
+                    break;
+                }
+            }
+        }
+        //get req params from user
+        String wish = request.getParameter("wish");
+        String clear = request.getParameter("clear");
 
-        //get req params
-       String wish = request.getParameter("wish");
-       String clear = request.getParameter("clear");
-       
-       
-       
-       //if clear was set, expire cokkie
-       if(clear != null){
-           
-       }else{
-             //if new wish item was set, append it to the current list
-             if(wish != null && !wish.isEmpty()){
-                 
-             }else {
-             //no wish item, get the current wish list only
-             }
-       }
-       
-     
-           
+        //if clear was set, expire cokkie
+        if (clear != null) {
+            
+            //for checking null when the use push clear button mutiple time
+            if (cookie != null) {
+                out.println("clear cookie");
+                
+                //expire th cookie immediately  
+                
+                cookie.setMaxAge(0);
+                
+                // the server has to tell client the cookie is expired
+                response.addCookie(cookie);
+            }
+        } else {
+            // temp var to store cookie value
+            String wishList = null;
+            
+            //if new wish item was set, append it to the current list
+            if (wish != null && !wish.isEmpty()) {
+                
+                //first time to add wish item to cookie, create one
+                if (cookie == null) {
+                    cookie = new Cookie("wishList", wish);
+
+                    wishList = wish;
+                } else {
+                    
+                    // append new item to existing wish list
+                    //first, get the previous cookie value
+                    wishList = cookie.getValue();
+                    
+                    // 2nd, add new wish with delimiter (~)
+                    wishList += "~" + wish;
+                    
+                    //must update cookie value
+                    cookie.setValue(wishList);
+                    
+                    //debug
+                    out.println("New Cookie value: " + wishList);
+
+                }
+                //update cookie object //set time 48 hours
+                cookie.setMaxAge(48 * 60 * 60);                
+                response.addCookie(cookie);
+                
+                //no wish item, get the current wish list only
+            } else 
+            if (cookie != null) {
+                
+                //get the prev. wish list
+                wishList = cookie.getValue();
+                out.println("Current Cookie value: " + wishList);
+            }
+        }
+
     }
-
 
 }
